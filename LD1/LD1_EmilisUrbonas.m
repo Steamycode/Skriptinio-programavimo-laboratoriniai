1e06945aca6da6612ac95367af700a63a1100141
Steamycode <e.u.regis1@gmail.com>
2026-09-09 17:22:02 +0300

    Update LD1_EmilisUrbonas.m

--- LD1/LD1_EmilisUrbonas.m
+++ LD1/LD1_EmilisUrbonas.m
@@ -7,3 +7,17 @@ plot(x, y, 'o-r', x, y/3, 'xb')
 title('Dvi funkcijos')
 xlabel('X-ai')
 ylabel('F_1 [-o-]   |   F_2 [-x-]')
+
+N = 8
+
+v = N+1:0.5:N+4
+
+A = [N N+1 N+2; N+3 N+4 N+5; N+6 N+7 N+8]
+
+A(3,2)
+A(2:3, 1:2)
+A([1 3 7 9])
+
+a = v(1:3)
+
+B = [A a']
